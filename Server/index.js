d1d5841fe81521f21b44a77670b209b3dde3ae6d
Yggdrasil09 <karthik99th.tnk@gamil.com>
2018-12-27 00:14:09 +0530

    The hosting and action of request is done

--- Server/index.js
+++ Server/index.js
@@ -4,11 +4,22 @@ const ytdl = require('ytdl-core');
 
 const app = express();
 
+app.use(cors());
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname +'/index.html');
+});
+
 app.listen(4000,()=>{
     console.log('Server listening to port 4000 on localhost');
 });
 
-app.get('/download',(req,res)=> {
+app.get('/download',(req,res) => {
     var URL = req.query.URL;
-    res.json({url : URL});
+    res.header('Content-Disposition','attachment;filename="video.mp4"');
+    ytdl(URL,{
+        format : 'mp4'
+    }).pipe(res);
 });
